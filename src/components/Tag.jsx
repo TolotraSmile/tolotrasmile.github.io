2ca7a8bfa011cb5d9d700150837cbf9167b3f7fd
TolotraSmile <tolotra.raharison@gmail.com>
2018-03-16 11:25:00 +0300

    Refresh source

--- src/components/Tag.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import classNames from 'classnames'
-
-const Tag = ({ name }) => {
-  return <span className={classNames('tag', `tag__${name.toLowerCase()}`)}>{name}</span>
-}
-
-export default Tag
